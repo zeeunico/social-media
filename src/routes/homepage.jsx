e9807bee783ba24407d990ea36581c8bbef58df1
RaihanEXE99 <brainsta@brainstation.com.bd>
2024-01-03 19:58:57 +0600

    header improvement

--- src/routes/homepage.jsx
+++ src/routes/homepage.jsx
@@ -1,9 +1,11 @@
+import RightSideBarMessage from "../components/sidebar/right-sidebar/right-sidebar-message"
 import LeftSideBarIcons from "../components/sidebar/side-bar-icons"
 
 function Homepage() {
   return (
     <div className="h-full">
       <LeftSideBarIcons />
+      <RightSideBarMessage />
     </div>
   )
 }
