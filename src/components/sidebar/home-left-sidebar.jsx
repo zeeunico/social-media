3a7681c3f69bcc2c7345f314ee6904d519007d71
Sadman Sakib Jisan <65308095+JisanEAE77@users.noreply.github.com>
2024-01-03 19:55:32 +0600

    Header and sidebar icons css fixed
    Fixed: Active icon has no border and bg without active-btn css class
    
    Fixed: Home icon has only fill with fixed color, but need white in general when not active, so added stroke
    
    Added: Sidebar menu and header icons hover effect changing icon stroke color and background gradient
    
    Fixed: Search box unnecessary outline on focus
    
    Added: on focus border color change added
    
    Added: Create button hover effect added changing bg color, text color and plus icon stroke color

---
+++ src/components/sidebar/home-left-sidebar.jsx
@@ -0,0 +1,12 @@
+// Jisan Is working
+
+const HomeLeftSideBar = () => {
+
+    return (
+        <>
+        <h1>Testing....</h1>
+        </>
+    );
+}
+
+export default HomeLeftSideBar;
--- src/routes/homepage.jsx
+++ src/routes/homepage.jsx
@@ -1,10 +1,17 @@
+import HomeLeftSideBar from "../components/sidebar/home-left-sidebar"
 import LeftSideBarIcons from "../components/sidebar/side-bar-icons"
+
 
 function Homepage() {
   return (
-    <div className="h-full">
-      <LeftSideBarIcons />
-    </div>
+    <>
+      <div className="h-full">
+        <LeftSideBarIcons />
+      </div>
+      {/* <div className="h-full">
+        <HomeLeftSideBar />
+      </div> */}
+    </>
   )
 }
 
